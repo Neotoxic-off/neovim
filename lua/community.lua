661bff159584cfedd60aa03e02555bc5c317aa0a
Paul Gardien <paulgardien@hoortrade.com>
2026-02-24 14:17:14 +0100

    🌱 feature: remove lsp auto format

--- lua/community.lua
+++ lua/community.lua
@@ -7,12 +7,12 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.dracula-nvim" },
   { import = "astrocommunity.colorscheme.night-owl-nvim" },
-  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
   -- import/override with your plugins folder
 }
